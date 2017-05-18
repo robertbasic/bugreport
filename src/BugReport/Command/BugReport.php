0456582c8fbdcec08e24f3ed5ac3ffdf14911552
Robert Basic <robertbasic.com@gmail.com>
2017-05-18 08:35:16 +0200

    Require argument for command

--- src/BugReport/Command/BugReport.php
+++ src/BugReport/Command/BugReport.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace BugReport\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -13,11 +14,14 @@ class BugReport extends Command
     {
         $this->setName('bugreport')
             ->setDescription('Create a bug report.')
-            ->setHelp('bugreport user/repo');
+            ->setHelp('bugreport user/repo')
+            ->addArgument('dependency', InputArgument::REQUIRED, 'Project dependency or composer.json file');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Hello.');
+
+        $dependency = $input->getArgument('dependency');
     }
 }
